27af0f91ebdcb88a65686c64c13a9428c3157da6
Maxwell Ding <mbding@gmail.com>
2019-10-29 02:59:07 -0500

    Defined player movement and boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
+    [SerializeField] private float move_speed = 10f;
+    [SerializeField] private float x_padding = 1f, y_padding = 1f;
+    private float x_min, x_max, y_min, y_max;
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        SetMoveBoundaries();
+    }
+
+    private void SetMoveBoundaries() {
+        Camera game_camera = Camera.main;
+        x_min = game_camera.ViewportToWorldPoint(new Vector3(0,0,0)).x + x_padding;
+        x_max = game_camera.ViewportToWorldPoint(new Vector3(1,0,0)).x - y_padding;
+
+        y_min = game_camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + y_padding;
+        y_max = game_camera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - y_padding;
     }
 
     // Update is called once per frame
@@ -16,7 +27,12 @@ public class Player : MonoBehaviour
     }
 
     private void Movement() {
-        var new_x_pos = transform.position.x + delta_X,
-            delta_X = ;
+        var delta_X = Input.GetAxis("Horizontal") * Time.deltaTime * move_speed;
+        var delta_Y = Input.GetAxis("Vertical") * Time.deltaTime * move_speed;
+        
+        var new_X_pos = Mathf.Clamp(transform.position.x + delta_X, x_min, x_max);
+        var new_Y_pos = Mathf.Clamp(transform.position.y + delta_Y, y_min, y_max);
+        
+        transform.position = new Vector2(new_X_pos, new_Y_pos);
     }
 }
