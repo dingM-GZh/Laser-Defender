30b9a98ed662a9b3db8dec398d03c24ab8a24c95
dingM-GZh <mbding@gmail.com>
2019-10-30 18:28:55 -0500

    Added waypoints and set up pathing

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+    [SerializeField] private List<Transform> waypoints;
+
+    [SerializeField] private float move_speed = 2f;
+
+    private int waypoint_index = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypoint_index].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (waypoint_index <= waypoints.Count - 1) {
+            var target_position = waypoints[waypoint_index].transform.position;
+            var frame_movement = move_speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position,target_position, frame_movement);
+
+            if (transform.position == target_position)
+                waypoint_index++;
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+}
